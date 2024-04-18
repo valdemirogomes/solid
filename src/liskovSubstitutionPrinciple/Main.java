deb29ce57f3bcfbd445e3394285074b6c262ecf0
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-04-18 00:23:41 -0300

    Liskov Substitution Principle

--- src/liskovSubstitutionPrinciple/Main.java
+++ src/liskovSubstitutionPrinciple/Main.java
@@ -3,8 +3,13 @@ package liskovSubstitutionPrinciple;
 public class Main {
 
 	public static void main(String[] args) {
-		Aluno nivelSuperior = new NivelSuperior("Chiquinho", 697796, "Medicina");
-		nivelSuperior.imprimirDados();
+		Aluno nivelSuperior1 = new NivelSuperior("Chiquinho", 6977968, "Medicina");
+		Aluno nivelSuperior2 = new NivelSuperior("Francisco", 2514326, "Odontologia");
+		nivelSuperior1.imprimirDados();
+		nivelSuperior2.imprimirDados();
+
+		
+		
 
 	}
 
--- src/liskovSubstitutionPrinciple/NivelSuperior.java
+++ src/liskovSubstitutionPrinciple/NivelSuperior.java
@@ -6,7 +6,12 @@ public class NivelSuperior extends Aluno {
 		super(nome, matricula, curso);
 		// TODO Auto-generated constructor stub
 	}
-	
+	@Override
+	public void imprimirDados() {
+		System.out.println("Nome: "+super.getNome());
+		System.out.println("Nome: "+super.getMatricula());
+		System.out.println("Nome: "+super.getCurso());
+	}
 
 
 }
