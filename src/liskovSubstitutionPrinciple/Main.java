f0352ae7e0923ec537e3c10b91b1c49ae1a9eb7c
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-04-19 12:54:05 -0300

    Liskov Substitution Principle

--- src/liskovSubstitutionPrinciple/Main.java
+++ src/liskovSubstitutionPrinciple/Main.java
@@ -3,8 +3,8 @@ package liskovSubstitutionPrinciple;
 public class Main {
 
 	public static void main(String[] args) {
-		Aluno nivelSuperior1 = new NivelSuperior("Chiquinho", 6977968, "Medicina");
-		Aluno nivelSuperior2 = new NivelSuperior("Francisco", 2514326, "Odontologia");
+		Aluno nivelSuperior1 = new NivelSuperior("Chiquinho", 6977968, "Medicina","Graduação","01/02/2005");
+		Aluno nivelSuperior2 = new NivelSuperior("Francisco", 2514326, "Odontologia","Graduação","01/02/2005");
 		nivelSuperior1.imprimirDados();
 		nivelSuperior2.imprimirDados();
 
--- src/liskovSubstitutionPrinciple/NivelSuperior.java
+++ src/liskovSubstitutionPrinciple/NivelSuperior.java
@@ -1,16 +1,23 @@
 package liskovSubstitutionPrinciple;
 
 public class NivelSuperior extends Aluno {
+	private String formacao;
+	private String dataInicio;
+	
 
-	public NivelSuperior(String nome, int matricula, String curso) {
+	public NivelSuperior(String nome, int matricula, String curso, String formacao, String dataInicio) {
 		super(nome, matricula, curso);
-		// TODO Auto-generated constructor stub
+		this.formacao = formacao;
+		this.dataInicio = dataInicio;
 	}
+
 	@Override
 	public void imprimirDados() {
 		System.out.println("Nome: "+super.getNome());
 		System.out.println("Nome: "+super.getMatricula());
 		System.out.println("Nome: "+super.getCurso());
+		System.out.println("Nome: "+this.formacao);
+		System.out.println("Nome: "+this.dataInicio);
 	}
 
 
